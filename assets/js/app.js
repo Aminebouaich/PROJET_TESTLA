cc1f7e092d3798a220606178d276b6865aa2e357
Aminebouaich <amine.stunteur15@gmail.com>
2022-04-15 17:07:27 +0200

    transition js

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,4 +6,53 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  const ratio =.1
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+  }
+
+  const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry)  {
+      if (entry.intersectionRatio > ratio) {
+        entry.target.classList.add('reveal-visible')
+      
+      observer.unobserve(entry.target)
+    } 
+    
+    })
+  }
   
+  
+  const observer = new IntersectionObserver(handleIntersect, options);
+  document.querySelectorAll('[class*="reveal-"]').forEach(function (r) { 
+    observer.observe(r)
+  })
+
+
+
+
+
+
+
+// const options = {
+//   root: null,
+//   rootMargin: '0px',
+//   threshold: ratio
+// }
+
+// const handleIntersect = function (entries, observer)  {
+//  entries.forEach(function (entry) {
+
+//   if (entry.IntersectionObserver > threshold) {
+//     entry.target.classList.add('reveal-visible')
+//     observer.unobserve(entry.target)
+//   }
+//  })
+ 
+// }
+
+// const observer = new IntersectionObserver(handleIntersect, options);
+// observer.observe(document.querySelector('.reveal'))
